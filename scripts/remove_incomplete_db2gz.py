380c57863f91fbc23af45805d9c53e19b3528f60
Ryan G. Coleman <ryan.g.coleman@gmail.com>
2014-01-30 21:55:29 -0800

    script for fixing broken docking database files

---
+++ scripts/remove_incomplete_db2gz.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+#due to stupid cluster issues, old code produces these
+#X      1003  30    351   +6.8084   -3.1632   -7.8126                            
+# X      1004  31    351   +5.7896   -2.640M     ZINC52473197        98  
+#find them and remove them
+
+import string, sys, os, shutil, gzip #i need these, i need them all
+
+if len(sys.argv) > 1:
+  inputFiles = sys.argv[1:]
+else:
+  print 'no .db2.gz files given!'
+  sys.exit(1)
+for input in inputFiles:
+  print "running", input, " now"
+  try:
+    db2file = gzip.open(input, 'rb')
+    outFileName = 'temp.donotusethisnameeverwhowoulddotha2t.db2.gz'
+    outFile = gzip.open(outFileName, 'wb', 9)
+    recordLines = []
+    for lineCount, line in enumerate(db2file):
+      if 0 == len(recordLines):
+        recordLines.append(line)
+      elif line.find('M     ZINC') != -1: #this is bad, just dump it.
+        position = line.find('M     ZINC')
+        recordLines = [line[position:]] #dump everything before this, start over
+        print "broken file", input, " repaired at line", lineCount
+      elif line[0] == 'E': #end of one record
+        recordLines.append(line)
+        for thisLine in recordLines:
+          outFile.write(thisLine)
+        recordLines = []
+      else:
+        recordLines.append(line)
+  except StopIteration:
+    pass
+  db2file.close()
+  outFile.close()
+  shutil.copy(outFileName, input)
+  os.unlink(outFileName)
+    
